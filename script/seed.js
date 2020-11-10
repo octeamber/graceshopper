ee555b96a6bd942c1aeed3faad576a8a81ec8a31
Laura Maranto <lauramaranto18@gmail.com>
2020-11-10 13:34:59 -0800

    requires removed

--- script/seed.js
+++ script/seed.js
@@ -30,37 +30,36 @@ const items = [
     price: 40,
     description: 'A lidded jar for salt on your counter.',
     qty: null,
-    imageUrl: require('../public/images/placeholder.jpg')
+    imageUrl: '../public/images/placeholder.jpg'
   },
   {
     name: 'Butter jar',
     price: 45,
     description: 'A lidded jar for storing butter.',
     qty: null,
-    imageUrl: require('../public/images/placeholder.jpg')
+    imageUrl: '../public/images/placeholder.jpg'
   },
   {
     name: 'Matcha Cup',
     price: 26,
     description: 'A light weight cup for matcha.',
     qty: null,
-    imageUrl: require('../public/images/placeholder.jpg')
+    imageUrl: '../public/images/placeholder.jpg'
   },
   {
     name: 'Lipped Cereal Bowl',
     price: 30,
     description: 'A big bowl for cereal.',
     qty: null,
-    imageUrl: require('../public/images/placeholder.jpg')
+    imageUrl: '../public/images/placeholder.jpg'
   },
   {
     name: 'Low Chicken Bowl',
     price: 45,
     description: 'A low serving bowl.',
     qty: null,
-    imageUrl: require('../public/images/placeholder.jpg')
-  },
-  {}
+    imageUrl: '../public/images/placeholder.jpg'
+  }
 ]
 
 async function seed() {
@@ -71,6 +70,12 @@ async function seed() {
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
+
+  const items = await Promise.all(
+    items.map(item => {
+      return Item.create(item)
+    })
+  )
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
