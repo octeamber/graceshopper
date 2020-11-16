055a80640555af445eaf9555c7bfd9eeaf34a644
Laura Maranto <lauramaranto18@gmail.com>
2020-11-16 14:14:41 -0800

    add to cart route revised

---
+++ server/api/orders.spec.js
@@ -0,0 +1,32 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Order = db.model('order')
+
+describe('Order routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/orders/', () => {
+    const inCart = false
+
+    beforeEach(() => {
+      return Order.create({
+        ordered: false
+      })
+    })
+
+    it('GET /api/orders', async () => {
+      const res = await request(app)
+        .get('/api/orders')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].ordered).to.be.equal(inCart)
+    })
+  }) // end describe('/api/orders')
+}) // end describe('User routes')
