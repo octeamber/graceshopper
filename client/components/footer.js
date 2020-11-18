ed0f9afee30523933e646e9c323ee96b5f28c866
Laura Maranto <lauramaranto18@gmail.com>
2020-11-17 17:15:06 -0800

    css font colors

---
+++ client/components/footer.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {GoMarkGithub} from 'react-icons/go'
+import {AiOutlineLinkedin} from 'react-icons/ai'
