03e1ab63a56ddeb8722f1972755cd528d9d42d7e
groana <groana7@gmail.com>
2021-03-07 16:59:06 -0500

    refactor: passing user test

--- server/api/users.js
+++ server/api/users.js
@@ -2,17 +2,7 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-// SECURE ROUTES
-const forAdmin = (req, res, next) => {
-  if (!req.user || !req.user.isAdmin) {
-    const err = new Error('This page is only available to admins!')
-    err.status = 401
-    return next(err)
-  }
-  next()
-}
-
-router.get('/', forAdmin, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
